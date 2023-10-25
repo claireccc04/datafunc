18d47e962839a045fc3f19835ffb23af15816c09
claire <jingc4878@gmail.com>
2023-10-25 14:52:36 +0100

    add comment

--- datafunc.R
+++ datafunc.R
@@ -1,6 +1,6 @@
 data <- function(x) {
   x <- x %>%
-    mutate(total = x[,1] + x[,2],
+    mutate(total = x[,1] + x[,2], #orignial argument
            average = (x[,1]+x[,2]+x[,3]+x[,4])/4) #second argument on dev branch
   return(x)
 }
